ea8cefdeae6d3ecbaf814d240f7800d9d106fee4
jonathan-oberlander <j.oberlander@novafutur.com>
2021-03-13 22:59:52 +0000

    setup passport js and jwt bearer strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -8,13 +10,16 @@ export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCrendentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCrendentialsDto);
   }
 
-  async signIn(authCrendentialsDto: AuthCredentialsDto) {
+  async signIn(
+    authCrendentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const username = await this.userRepository.validateUserPassword(
       authCrendentialsDto,
     );
@@ -22,5 +27,10 @@ export class AuthService {
     if (!username) {
       throw new UnauthorizedException('Invalid Credentials');
     }
+
+    const payload: JwtPayload = { username };
+    const accessToken = this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
